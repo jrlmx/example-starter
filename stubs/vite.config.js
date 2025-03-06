6ffefe4e6a514da5549801351bd628ceb2d73156
JRLmx <bocaj.xml@gmail.com>
2025-03-06 15:10:56 -0500

    wip

--- stubs/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import laravel from '@vitejs/plugin-laravel'
-import tailwindcss from 'vite-plugin-tailwind'
-
-export default defineConfig({
-    plugins: [tailwindcss(), laravel(['resources/css/app.css', 'resources/js/app.js'])]
-})
